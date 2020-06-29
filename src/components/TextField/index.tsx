d009b3d906a195aaa004ae81c5242b9bf0beb3cf
pedrolauro15 <pedrolauro15@gmail.com>
2020-06-29 13:39:21 -0300

    Adding textField props

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -6,6 +6,11 @@ export interface TextFieldProps {
 	elementColor?: string;
 	label?: string;
 	highlight?: boolean;
+	onChange?(): void | undefined;
+	value?: string;
+	defaultValue?: string;
+	onFocus?(): void;
+	onBlur?(): void;
 }
 
 export type InputProps = JSX.IntrinsicElements['input'] & TextFieldProps;
@@ -15,11 +20,24 @@ export function TextField({
 	elementColor,
 	label,
 	highlight,
-	...rest
-}: InputProps) {
+	onChange,
+	onBlur,
+	onFocus,
+	value,
+	defaultValue,
+}: TextFieldProps) {
 	return (
 		<InputField color={color}>
-			<Input elementColor={elementColor} required name="user" />
+			<Input
+				onFocus={onFocus}
+				onBlur={onBlur}
+				onChange={onChange}
+				elementColor={elementColor}
+				required
+				name="materialized-input"
+				value={value}
+				defaultValue={defaultValue}
+			/>
 			{highlight && <Highlight className="highlight" />}
 			<Bar className="bar" color={color} />
 			<Label>{label}</Label>
